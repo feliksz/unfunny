61ea7d1d6e98d7ff3725cbd29086d123d9839cac
feliksz <43099789+feliksz@users.noreply.github.com>
2020-08-23 09:49:10 +0200

    posortowalem ten syf alfabetycznie

--- polando.hpp
+++ polando.hpp
@@ -1,38 +1,41 @@
 #pragma once
 #include <iostream>
 
-#define JEZELI if
+// TO-DO: dodac char(przetlumaczyc)
 #define ALBO else
-#define ZWROC return
-#define WYBOR switch
-
-#define WYBOR_WARTOSC case
-#define WYBOR_KONTYNUUJ continue
-#define WYBOR_ZATRZYMAJ break 
-
-#define POKAZ std::cout
-#define WPROWADZ std::cin
-#define ZAKONCZ_LINIE std::endl
-
-#define UZYWA using
-#define W_LINII inline
+#define BEZ_ZNAKU unsigned
 #define DEFINICJA_TYPU typedef
-
-#define STALY const
-#define STATYCZNY static
-
-#define PROZNIA void
-#define OPERATOR_LOGICZNY bool
-
-#define LICZBA_CALKOWITA8 signed char
+#define DLUGA long //XDXDXDXD
+#define FALSZ false
+#define JEZELI if
+#define KIEDY while
+#define KLASA class
 #define LICZBA_CALKOWITA16 short
 #define LICZBA_CALKOWITA32 int
 #define LICZBA_CALKOWITA64 long long
-
-#define LICZBA_CALKOWITA_BEZ_ZNAKU8 unsigned char
+#define LICZBA_CALKOWITA8 signed char
 #define LICZBA_CALKOWITA_BEZ_ZNAKU16 unsigned short
 #define LICZBA_CALKOWITA_BEZ_ZNAKU32 unsigned int
 #define LICZBA_CALKOWITA_BEZ_ZNAKU64 unsigned long long
-
+#define LICZBA_CALKOWITA_BEZ_ZNAKU8 unsigned char
 #define LICZBA_ZMIENNOPRZECINKOWA float
 #define LICZBA_ZMIENNOPRZECINKOWA_PODWOJNEJ_PRECYZJI double
+#define OPERATOR_LOGICZNY bool
+#define PETLA_FOR for
+#define POKAZ std::cout
+#define PRAWDA true
+#define PROZNIA void
+#define PRZESTRZEN_NAZW namespace
+#define PUBLICZNE public
+#define STALY const
+#define STATYCZNY static
+#define UZYWA using
+#define WPROWADZ std::cin
+#define WYBOR switch
+#define WYBOR_KONTYNUUJ continue
+#define WYBOR_WARTOSC case
+#define WYBOR_ZATRZYMAJ break
+#define W_LINII inline
+#define ZAKONCZ_LINIE std::endl
+#define ZEWNETRZNE extern
+#define ZWROC return
